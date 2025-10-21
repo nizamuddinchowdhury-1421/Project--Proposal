f7d633f8b1a3483164128922816cbb16fef1bdd8
fabiaborsha <155887848+fabiaborsha@users.noreply.github.com>
2025-10-21 23:58:46 +0600

    Delete centers directory

--- centers/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import ServiceCenter
-
-
-def center_list(request):
-    centers = ServiceCenter.objects.filter(is_active=True)
-    return render(request, 'centers/center_list.html', {'centers': centers})
-
-# Create your views here.
